bf91cfdb3d295560b08c7451655beeac1cfa4753
Lucas Henrique Trevelin <lucas-henrique_venturus>
2025-03-19 09:33:10 -0300

    feat: add custom async validator

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,4 +1,5 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { EmailEhUnico } from '../validacao/email-eh-unico.validator';
 
 export class CriaUsuarioDTO {
   @IsNotEmpty({ message: 'Nome é obrigatório' })
@@ -6,6 +7,7 @@ export class CriaUsuarioDTO {
   nome: string;
 
   @IsEmail(undefined, { message: 'Email inválido' })
+  @EmailEhUnico({ message: 'Já existe um usuário com este e-mail' })
   email: string;
 
   @MinLength(6, { message: 'Senha deve ter no mínimo 6 caracteres' })
